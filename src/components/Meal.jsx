49e13a78bc860565c2bc6240abf80c07d88e7400
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2026-06-30 18:49:14 +0100

    fix meal item markup

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -1,14 +1,18 @@
 const Meal = ({ name, price, description, img }) => {
   return (
-    <div className='meal-item'>
-      <img src={img} alt={name} />
-      <h3>{name}</h3>
-      <p className='meal-item-price'>£{price}</p>
-      <p className='meal-item-description'>{description}</p>
-      <div className='meal-item-actions'>
-        <button type='button'>Add to Cart</button>
-      </div>
-    </div>
+    <li className='meal-item'>
+      <article>
+        <img src={img} alt={name} />
+        <div>
+          <h3>{name}</h3>
+          <p className='meal-item-price'>£{price}</p>
+          <p className='meal-item-description'>{description}</p>
+        </div>
+        <p className='meal-item-actions'>
+          <button type='button'>Add to Cart</button>
+        </p>
+      </article>
+    </li>
   );
 };
 
