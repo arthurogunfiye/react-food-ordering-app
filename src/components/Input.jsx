b4b4f50fd40964a2f7f1228e23088d3c205fe592
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2026-07-10 16:16:19 +0100

    implement custom input component and manage modal visibility

---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+const Input = ({ label, id, ...props }) => {
+  return (
+    <p className='control'>
+      <label htmlFor={id}>{label}</label>
+      <input type='text' id={id} name={id} {...props} required />
+    </p>
+  );
+};
+
+export default Input;
